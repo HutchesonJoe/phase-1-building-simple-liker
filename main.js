e353c818ee8aae5892479e237252b76d548865c7
Joe <hutcheson2001@yahoo.com>
2022-01-29 13:10:47 -0500

    commit tree b599f73f8a979d9e0dbdae0254233058d316e339

--- main.js
+++ main.js
@@ -4,8 +4,35 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let buttons = document.querySelectorAll(".like-glyph");
+for (let button of buttons) {
+  button.addEventListener("click", handleClick)
+}
 
 
+
+function handleClick(event){
+  const heart = event.target
+  // console.log(heart)
+  mimicServerCall()
+  .then (function(){
+    if (heart.textContent === EMPTY_HEART) {
+      heart.textContent = FULL_HEART
+      heart.className = "activated-heart"
+    }
+    else {
+      heart.textContent = EMPTY_HEART
+      heart.className = ''
+    }
+// console.log("we made it")
+  })
+
+
+  .catch ((error) => {
+    let error2 = document.querySelector("#modal")
+    error2.className = ''
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
